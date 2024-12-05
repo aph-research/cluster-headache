fd99487be295d81d07d6335643e3ea166918d77f
Alfredo Parra <30336042+aph-research@users.noreply.github.com>
2024-12-05 08:28:08 +0000

    Increased MS mean to account for trigeminal neuralgia

--- SimulationConfig.py
+++ SimulationConfig.py
@@ -17,7 +17,7 @@ class SimulationConfig:
     base: float = np.e
     n_taylor: int = 2
     scaling_factor: float = 1.0
-    ms_mean: float = 1.5
+    ms_mean: float = 2.0
     ms_median: float = 3.5
     ms_std: float = 1.8
     ms_prevalence_per_100k: int = 37
